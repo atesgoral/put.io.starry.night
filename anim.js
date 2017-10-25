695ed59227e00bdf67868cad3cbe6aa836303376
Ates Goral <ates.g@myplanet.com>
2017-10-24 20:13:10 -0400

    New logo, datgui, paths

--- anim.js
+++ anim.js
@@ -1,4 +1,44 @@
 window.onload = function () {
+  var config = {
+    logo: {
+      scale: 0.66
+    },
+    paths: [{
+      enabled: true,
+      speed: 0.2,
+      horizPos: 0,
+      vertPos: 0.3,
+      length: 0.5,
+      phase: 0.5,
+      period: 1,
+      amplitude: 0.15,
+      amplitudeJitter: 0.25,
+      horizJitter: 0.1
+    }, {
+      enabled: true,
+      speed: -0.2,
+      horizPos: 0.5,
+      vertPos: 0.7,
+      length: 0.5,
+      phase: 0,
+      period: 1,
+      amplitude: 0.15,
+      amplitudeJitter: 0.25,
+      horizJitter: 0.1
+    }, {
+      enabled: false,
+      speed: 0.27,
+      horizPos: 0.13,
+      vertPos: 0.43,
+      length: 0.39,
+      phase: 1,
+      period: 0.5,
+      amplitude: 0.34,
+      amplitudeJitter: 0.31,
+      horizJitter: 0.12
+    }]
+  };
+
   var MIN_RADIUS = 5;
   var MAX_RADIUS = 12;
 
@@ -10,20 +50,14 @@ window.onload = function () {
     return MIN_RADIUS + (1 + dot.r) * (MAX_RADIUS - MIN_RADIUS) / 2;
   }
 
-  function createPath(startP, startY, startX, endX, period, amplitute) {
-    var path = {
-      startP: startP,
-      startY: startY,
-      startX: startX,
-      len: endX - startX,
-      period: period,
-      amplitute: amplitute,
-      dots: []
-    };
-
+  function createDots(path) {
     var x = 0;
 
-    while (x < path.len) {
+    path.dots = [];
+
+    var maxX = path.length * canvas.width;
+
+    while (x < maxX) {
       var dot = {
         a: r(), // amplitute
         r: r() // radius
@@ -32,17 +66,15 @@ window.onload = function () {
       var radius = getRadius(dot);
       var dotX = x + radius;
 
-      dot.p = dotX / path.len;
+      dot.p = dotX / maxX; // phase
 
       path.dots.push(dot);
 
-      x += radius * 2 + Math.random() * path.len / 50;
+      x += radius * 2 + Math.random() * path.horizJitter * path.horizJitter * canvas.width;
     }
-
-    return path;
   }
 
-  var SCALE = 2;
+  var SCALE = 2; // @todo not used yet
   var canvas = document.getElementById('anim');
 
   canvas.width = canvas.offsetWidth * 2;
@@ -51,15 +83,10 @@ window.onload = function () {
   var ctx = canvas.getContext('2d');
 
   var logo = new Image();
-  logo.src = 'putio-logo-vektor-transparent.png';
+  logo.src = 'logo.png';
 
   logo.onload = function () {
     var logoAspectRatio = logo.width / logo.height;
-
-    var paths = [
-      createPath(Math.PI, canvas.height * .1, 0, canvas.width / 2, Math.PI * 2, 100),
-      createPath(0, canvas.height * .75, canvas.width / 2, canvas.width, Math.PI * 2, 60)
-    ];
 
     ctx.fillStyle = '#fff';
     ctx.globalCompositeOperation = 'xor';
@@ -71,23 +98,43 @@ window.onload = function () {
 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+      var logoW = logo.width * config.logo.scale;
+      var logoH = logoW / logoAspectRatio;
+
       ctx.drawImage(
         logo,
         0, 0, logo.width, logo.height,
-        0, -canvas.height / 2, canvas.width, canvas.width / logoAspectRatio
+        (canvas.width - logoW) / 2, (canvas.height - logoH) / 2, logoW, logoH
       );
 
-      for (var i = 0; i < paths.length; i++) {
-        var path = paths[i];
+      for (var i = 0; i < config.paths.length; i++) {
+        var path = config.paths[i];
+
+        if (!path.enabled) {
+          continue;
+        }
+
+        if (!path.dots) {
+          createDots(path);
+        }
+
+        var maxX = path.length * canvas.width;
+
+        var pos = ((t * Math.pow(path.speed, 3) % maxX) + maxX) % maxX;
 
         for (var j = 0; j < path.dots.length; j++) {
           var dot = path.dots[j];
 
-          // var x = (t / 10 + dot.p * canvas.width) % canvas.width;
-          // var y = (Math.sin(x / 200) + dot.a / 3) * 60 + canvas.height / 2;
-
-          var x = path.startX + (t / 40 + dot.p * path.len) % path.len;
-          var y = (Math.sin(x / 200) + dot.a + path.startP) * path.amplitute + path.startY;
+          var dotX = (pos + dot.p * maxX) % maxX;
+          var x = path.horizPos * canvas.width + dotX;
+          var y = (
+            (
+              Math.sin(
+                (dotX / maxX * path.period + path.phase) * Math.PI * 2
+              )
+            ) * path.amplitude + dot.a * path.amplitudeJitter * path.amplitudeJitter
+            + path.vertPos
+          ) * canvas.height;
 
           ctx.beginPath();
           ctx.arc(x, y, getRadius(dot), 0, Math.PI * 2);
@@ -98,4 +145,25 @@ window.onload = function () {
 
     requestAnimationFrame(repaint);
   };
+
+  var gui = new dat.GUI();
+
+  var logoFolder = gui.addFolder('Logo');
+  logoFolder.open();
+  logoFolder.add(config.logo, 'scale', 0, 1);
+
+  config.paths.forEach(function (path, idx) {
+    var folder = gui.addFolder('Path ' + (idx + 1));
+    folder.open();
+    folder.add(path, 'enabled');
+    folder.add(path, 'speed', -1, 1);
+    folder.add(path, 'horizPos', 0, 1);
+    folder.add(path, 'vertPos', 0, 1);
+    folder.add(path, 'length', 0, 1).onChange(function () { path.dots = null; });
+    folder.add(path, 'phase', 0, 1);
+    folder.add(path, 'period', 0, 1);
+    folder.add(path, 'amplitude', 0, 1);
+    folder.add(path, 'amplitudeJitter', 0, 1);
+    folder.add(path, 'horizJitter', 0, 1).onChange(function () { path.dots = null; });
+  });
 };
